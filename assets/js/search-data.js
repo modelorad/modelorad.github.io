3c4ffee7c8ea3745ed72153319dc3aac7c7ee1a8
devin409 <57831201+devin409@users.noreply.github.com>
2025-01-17 19:08:50 +0000

    Deploying to gh-pages from @ devin409/devin409.github.io@af1854a9c8bfd6f9ebed5d96264a00f28267e879 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,19 +421,19 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-final-report",
-          title: 'Final Report',
+          section: "News",},{id: "projects-project-application",
+          title: 'project application',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-media",
+          title: 'media',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-final-report",
+          title: 'final report',
+          description: "final report with redirect to website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
