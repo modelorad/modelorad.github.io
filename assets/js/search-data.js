2788d0de12db1bf4f81d5513a15b7da87f76cc60
modelorad <ivi.karlsruhe@gmail.com>
2025-01-21 15:12:50 +0000

    Deploying to gh-pages from @ modelorad/modelorad.github.io@237daa1e795d480d2a87397665f8b12425527d32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -265,20 +265,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.h-ka.de/ieem/projekte/modelo-rad", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
