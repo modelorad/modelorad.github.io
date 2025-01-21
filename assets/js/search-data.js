c99af46502475882e3d373056962106cbc61a479
devin409 <57831201+devin409@users.noreply.github.com>
2025-01-21 11:32:07 +0000

    Deploying to gh-pages from @ devin409/devin409.github.io@978f7bccf3173885cf719896918b69f259bca099 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -283,9 +283,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-media-publications",
-          title: 'media/publications',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-final-report",
