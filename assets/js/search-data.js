957a431b4c40b5065096f49e06cdd844fdfc4a24
modelorad <ivi.karlsruhe@gmail.com>
2025-02-05 15:40:58 +0000

    Deploying to gh-pages from @ modelorad/modelorad.github.io@e4bd473bdbd260bbc31d6ad8e4e84c8f02c8e029 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,28 +113,6 @@ ninja.data = [{
           window.location.href = "/posts/2024/Modelo3_TechnischeRealisierung-(DE)/";
         
       },
-    },{id: "post-a-post-with-echarts",
-      
-        title: "a post with echarts",
-      
-      description: "this is what included echarts code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/posts/2023/echarts/";
-        
-      },
-    },{id: "post-a-post-with-chart-js",
-      
-        title: "a post with chart.js",
-      
-      description: "this is what included chart.js code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/posts/2023/chartjs/";
-        
-      },
     },{id: "post-project-gallery",
       
         title: "project gallery",
